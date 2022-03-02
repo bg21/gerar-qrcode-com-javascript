1216403bdabf1887458b688f5a7d0b1fec04f694
Juliana Costa <juhcosta23@gmail.com>
2022-03-01 21:37:12 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var qrcode = new QRCode("qrcode");
+
+function makeCode() {
+  var elText = document.getElementById("text");
+  
+  qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+$("#text").
+on("blur", function () {
+  makeCode();
+}).
+on("keydown", function (e) {
+  if (e.keyCode == 13) {
+    makeCode();
+  }
+});
+$("#btn").
+on("click", function () {
+  makeCode();
+});
